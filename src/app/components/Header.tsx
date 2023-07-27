905804ef85a414805e4539c01a75f73ba62de7e2
ariel7198 <ariel.7198@hotmail.com>
2023-07-27 17:09:55 -0300

    optimizing header style

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 
 export default function Header () {
     return (
-        <div className='w-screen bg-red-500/50 flex justify-center items-center content-center z-10'>
-            <div className="max-w-7xl min-w-[80vw] justify-between items-center font-mono text-sm lg:flex ">
+        <div className={`${style.headerContainer} w-screen bg-white/60 flex justify-between items-center content-center z-10 fixed`}>
+            <div className="flex min-w-[100vw] justify-between items-center text-sm pl-10">
                 <div className='flex items-center'>
                     <div>
                         <Image
@@ -38,7 +38,7 @@ export default function Header () {
                     </div>
                     <div> 
                         <button className="btn-sm btn-circle flex justify-center content-center items-center mx-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" className="w-5 h-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" className="w-5 h-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                             </svg>
                         </button>
@@ -49,18 +49,21 @@ export default function Header () {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
                             </svg>
                             EN
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                        </svg>
                         </button>
                     </div>
                 </div>
                 <div className={`flex justify-center content-center items-center p-3 rounded-l-full ${style.avatarContainer}`}>
-                    <div className="avatar">
+                    <div className="avatar mx-2 p-2 rounded-full bg-white">
                         <div className="w-8 rounded-full">
                             <img src="/header/avatar.png" />
                         </div>
                     </div>
                     <div className="title">
-                        <p> trizky2022 </p>
-                        <small> 0x92831....124b </small>
+                        <p className='font-black'> trizky2022 </p>
+                        <small className='font-medium'> 0x92831....124b </small>
                     </div>
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
